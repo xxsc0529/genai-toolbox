83b87f46666ad83da918d0f8597614461f67b19d
Twisha Bansal <58483338+twishabansal@users.noreply.github.com>
2024-11-13 21:25:09 +0530

    chore(llamaindex-sdk): Fix type checks (#69)

--- sdks/llamaindex/src/toolbox_llamaindex_sdk/client.py
+++ sdks/llamaindex/src/toolbox_llamaindex_sdk/client.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import Optional
+from typing import Optional, Type
 
 from aiohttp import ClientSession
 from llama_index.core.tools import FunctionTool
@@ -90,7 +90,7 @@ class ToolboxClient:
             The generated tool.
         """
         tool_schema = manifest.tools[tool_name]
-        tool_model: BaseModel = _schema_to_model(
+        tool_model: Type[BaseModel] = _schema_to_model(
             model_name=tool_name, schema=tool_schema.parameters
         )
 
