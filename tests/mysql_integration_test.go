1764a4918ce167a9dddb97ac359dc2959207cf56
Yuan <45984206+Yuan325@users.noreply.github.com>
2025-01-31 10:54:00 -0800

    ci(mysql): add integration test (#252)
    Add integration test for `mysql`.

---
+++ tests/mysql_integration_test.go
@@ -0,0 +1,181 @@
+//go:build integration && mysql
+
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tests
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"reflect"
+	"regexp"
+	"testing"
+	"time"
+)
+
+var (
+	MYSQL_DATABASE = os.Getenv("MYSQL_DATABASE")
+	MYSQL_HOST     = os.Getenv("MYSQL_HOST")
+	MYSQL_PORT     = os.Getenv("MYSQL_PORT")
+	MYSQL_USER     = os.Getenv("MYSQL_USER")
+	MYSQL_PASS     = os.Getenv("MYSQL_PASS")
+)
+
+func requireMySQLVars(t *testing.T) {
+	switch "" {
+	case MYSQL_DATABASE:
+		t.Fatal("'MYSQL_DATABASE' not set")
+	case MYSQL_HOST:
+		t.Fatal("'MYSQL_HOST' not set")
+	case MYSQL_PORT:
+		t.Fatal("'MYSQL_PORT' not set")
+	case MYSQL_USER:
+		t.Fatal("'MYSQL_USER' not set")
+	case MYSQL_PASS:
+		t.Fatal("'MYSQL_PASS' not set")
+	}
+}
+
+func TestMySQL(t *testing.T) {
+	requireMySQLVars(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	var args []string
+
+	// Write config into a file and pass it to command
+	toolsFile := map[string]any{
+		"sources": map[string]any{
+			"my-mysql-instance": map[string]any{
+				"kind":     "mysql",
+				"host":     MYSQL_HOST,
+				"port":     MYSQL_PORT,
+				"database": MYSQL_DATABASE,
+				"user":     MYSQL_USER,
+				"password": MYSQL_PASS,
+			},
+		},
+		"tools": map[string]any{
+			"my-simple-tool": map[string]any{
+				"kind":        "mysql-sql",
+				"source":      "my-mysql-instance",
+				"description": "Simple tool to test end to end functionality.",
+				"statement":   "SELECT 1;",
+			},
+		},
+	}
+	cmd, cleanup, err := StartCmd(ctx, toolsFile, args...)
+	if err != nil {
+		t.Fatalf("command initialization returned an error: %s", err)
+	}
+	defer cleanup()
+
+	waitCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	out, err := cmd.WaitForString(waitCtx, regexp.MustCompile(`Server ready to serve`))
+	if err != nil {
+		t.Logf("toolbox command logs: \n%s", out)
+		t.Fatalf("toolbox didn't start successfully: %s", err)
+	}
+
+	// Test tool get endpoint
+	tcs := []struct {
+		name string
+		api  string
+		want map[string]any
+	}{
+		{
+			name: "get my-simple-tool",
+			api:  "http://127.0.0.1:5000/api/tool/my-simple-tool/",
+			want: map[string]any{
+				"my-simple-tool": map[string]any{
+					"description": "Simple tool to test end to end functionality.",
+					"parameters":  []any{},
+				},
+			},
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := http.Get(tc.api)
+			if err != nil {
+				t.Fatalf("error when sending a request: %s", err)
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				t.Fatalf("response status code is not 200")
+			}
+
+			var body map[string]interface{}
+			err = json.NewDecoder(resp.Body).Decode(&body)
+			if err != nil {
+				t.Fatalf("error parsing response body")
+			}
+
+			got, ok := body["tools"]
+			if !ok {
+				t.Fatalf("unable to find tools in response body")
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("got %q, want %q", got, tc.want)
+			}
+		})
+	}
+
+	// Test tool invoke endpoint
+	invokeTcs := []struct {
+		name        string
+		api         string
+		requestBody io.Reader
+		want        string
+	}{
+		{
+			name:        "invoke my-simple-tool",
+			api:         "http://127.0.0.1:5000/api/tool/my-simple-tool/invoke",
+			requestBody: bytes.NewBuffer([]byte(`{}`)),
+			want:        "Stub tool call for \"my-simple-tool\"! Parameters parsed: [] \n Output: [%!s(int64=1)]",
+		},
+	}
+	for _, tc := range invokeTcs {
+		t.Run(tc.name, func(t *testing.T) {
+			resp, err := http.Post(tc.api, "application/json", tc.requestBody)
+			if err != nil {
+				t.Fatalf("error when sending a request: %s", err)
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != 200 {
+				t.Fatalf("response status code is not 200")
+			}
+
+			var body map[string]interface{}
+			err = json.NewDecoder(resp.Body).Decode(&body)
+			if err != nil {
+				t.Fatalf("error parsing response body")
+			}
+			got, ok := body["result"].(string)
+			if !ok {
+				t.Fatalf("unable to find result in response body")
+			}
+
+			if got != tc.want {
+				t.Fatalf("unexpected value: got %q, want %q", got, tc.want)
+			}
+		})
+	}
+}
