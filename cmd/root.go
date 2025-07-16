ed5ef4caea10ba1dbc49c0fc0a0d2b91cf341d3b
prernakakkar-google <158031829+prernakakkar-google@users.noreply.github.com>
2025-07-16 11:48:24 +0000

    feat: Create wait for tool (#885)
    `
    tools:
      wait_for_tool:
        kind: wait-for
    description: "Tool puts chat to sleep for given timeout example 3m,10s,
    5m etc. For tasks such as cluster creation no need to wait. It takes
    longer time such as 8m for instance creation. Use timeout value as
    default if nothing is provided"
        timeout: 10s
    `
    
    Output: 
    `
    prernakakkar@prernakakkar:~/senseai/genai-toolbox$ curl -X POST -H
    "Content-Type: application/json" -d '{"duration": "5m"}'
    http://127.0.0.1:5000/api/tool/wa
    it_for_tool/invoke
    {"result":"[\"Wait for 5m0s completed successfully.\"]"}
    `

--- cmd/root.go
+++ cmd/root.go
@@ -64,6 +64,7 @@ import (
 	_ "github.com/googleapis/genai-toolbox/internal/tools/spanner/spannerexecutesql"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/spanner/spannersql"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/sqlitesql"
+	_ "github.com/googleapis/genai-toolbox/internal/tools/utility/wait"
 	_ "github.com/googleapis/genai-toolbox/internal/tools/valkey"
 
 	"github.com/spf13/cobra"
