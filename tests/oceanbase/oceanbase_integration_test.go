cb004790ba63ae95c6ce8a0b2216c63db79b5a99
Wenxin Du <117315983+duwenxin99@users.noreply.github.com>
2025-08-18 11:19:43 -0400

    Update oceanbase_integration_test.go

--- tests/oceanbase/oceanbase_integration_test.go
+++ tests/oceanbase/oceanbase_integration_test.go
@@ -165,7 +165,7 @@ func getOceanBaseTmplToolStatement() (string, string) {
 // OceanBase specific expected results
 func getOceanBaseWants() (string, string, string) {
 	select1Want := "[{\"1\":1}]"
-	failInvocationWant := `{\"jsonrpc\":\"2.0\",\"id\":\"invoke-fail-tool\",\"result\":{\"content\":[{\"type\":\"text\",\"text\":\"unable to execute query: Error 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your OceanBase version for the right syntax to use near 'SELEC 1;' at line 1\"}],\"isError\":true}}`
+	failInvocationWant := `{"jsonrpc":"2.0","id":"invoke-fail-tool","result":{"content":[{"type":"text","text":"unable to execute query: Error 1064 (42000): unable to execute query: Error 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your OceanBase version for the right syntax to use near 'SELEC 1;' at line 1"}],"isError":true}}`
 	createTableStatement := `"CREATE TABLE t (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))"`
 	return select1Want, failInvocationWant, createTableStatement
 }
